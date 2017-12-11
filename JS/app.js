8f6b9a49bca5b0b0af8c210b8664760cb8ef021b
Jack Durham <jackdurham11@gmail.com>
2017-12-11 10:32:44 +0000

    first working js

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,14 @@
-$(() => {
-  
+$(() =>{
+  const $el = $('.hoop');
+  let HDirection = '+';
+  setInterval(function(){
+    const left = parseInt($el.css('margin-left'));
+
+    //moving from left to right of the screen
+    if(left > $(window).width() )
+      HDirection = '-';
+    else if(left < 0 )
+      HDirection = '+';
+    $('.hoop').css('margin-left', `${HDirection}=1px`);
+  });
 });
