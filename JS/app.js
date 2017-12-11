53bf12acfa4740a32d445d5e57e5168de47de690
Jack Durham <jackdurham11@gmail.com>
2017-12-11 15:07:43 +0000

    ball moving up

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,20 @@
 $(() =>{
   const $el = $('.hoop');
+
   let HDirection = '+';
-  let VDirection = '+';
+
   setInterval(function(){
-    const left = parseInt($el.css('margin-left'));
+    const hoop = parseInt($el.css('margin-left'));
 
     //moving from left to right of the screen
-    if(left > $('body').width() - $el.width() )
+    if(hoop > $('body').width() - $el.width() )
       HDirection = '-';
-    else if(left < 0 )
+    else if(hoop < 0 )
       HDirection = '+';
     $('.hoop').css('margin-left', `${HDirection}=1px`);
 
-    if(top > $('body').height() + $el.height() )
-      VDirection = '+';
-    else if(top < 0 )
-      VDirection = '-';
-    $('.ball').css('margin-top', `${VDirection}=1px`);
+
+    $('.ball').css('margin-top','-=1px');
   });
 
 
