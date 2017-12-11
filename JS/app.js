28e9737b37df0bdf2a089f4b540b0e3c9d7a50d6
Jack Durham <jackdurham11@gmail.com>
2017-12-11 12:08:42 +0000

    working hoop

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,24 @@
 $(() =>{
   const $el = $('.hoop');
   let HDirection = '+';
+  let VDirection = '+';
   setInterval(function(){
     const left = parseInt($el.css('margin-left'));
 
     //moving from left to right of the screen
-    if(left > $('body').width() )
+    if(left > $('body').width() - $el.width() )
       HDirection = '-';
     else if(left < 0 )
       HDirection = '+';
     $('.hoop').css('margin-left', `${HDirection}=1px`);
+
+    if(top > $('body').height() )
+      VDirection = '-';
+    else if(top < 0 )
+      VDirection = '+';
+    $('.ball').css('margin-top', `${VDirection}=1px`);
   });
+
 
 
 
