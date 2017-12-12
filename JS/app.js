dd3298882782b118fb1fa31ef3a87f45b9d11478
Jack Durham <jackdurham11@gmail.com>
2017-12-12 10:50:33 +0000

    collison working

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,49 @@
 $(() =>{
-  const $el = $('.hoop');
+  const $hoop = $('.hoop');
+  const $ball =  $('.ball');
+  let $hoopOffset = null;
+  let $ballOffset = null;
 
   let HDirection = '+';
 
   setInterval(function(){
-    const hoop = parseInt($el.css('margin-left'));
+    const hoop = parseInt($hoop.css('margin-left'));
 
     //moving from left to right of the screen
-    if(hoop > $('body').width() - $el.width() )
+    if(hoop > $('body').width() - $hoop.width() )
       HDirection = '-';
     else if(hoop < 0 )
       HDirection = '+';
     $('.hoop').css('margin-left', `${HDirection}=1px`);
-
+    checkCollision();
   });
 
   $('button').click(function() {
-    $('.ball').animate({top: '160px'});
+    $('.ball').animate({top: '160px'}, function(){
+      console.log('done!');
+    });
 
   });
+  function checkCollision(){
+    $hoopOffset = $hoop.offset();
+    $ballOffset = $ball.offset();
+
+    const condition1 = ($ballOffset.left < $hoopOffset.left + $hoop.width() );
+
+    const condition2 = ($ballOffset.left + $ball.width() > $hoopOffset.left);
+
+    const condition3 = ($ballOffset.top < $hoopOffset.top + $hoop.height() );
+
+    const condition4 = ($ball.height() + $ballOffset.top > $hoopOffset.top);
+
+    if (condition1 && condition2 && condition3 && condition4){
+      console.log('score');
+    } else {
+      console.log('nope');
+    }
+  }
+
+
+
 
 });
